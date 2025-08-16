39e6818106a0fd0a1b8ca12bd5ba8566df59bc99
Aditya Kumar Yadav <120782791+aditya003-aky@users.noreply.github.com>
2025-08-16 22:16:59 +0530

    Update quick_sort.js

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Aditya Yadav
     
     *****************
 */
@@ -82,7 +82,7 @@ function quick_sort (start, end )
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Aditya Yadav
     
     *****************
 */
