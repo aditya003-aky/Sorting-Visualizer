fbc76040c8b6b919175017fb4ffd73b38afc37d6
Aditya Kumar Yadav <120782791+aditya003-aky@users.noreply.github.com>
2025-08-16 22:15:36 +0530

    Update insertion_sort.js

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Aditya Yadav
     
     *****************
 */
@@ -58,7 +58,7 @@ function Insertion()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Aditya Yadav
     
     *****************
 */
